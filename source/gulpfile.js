09bdedd9083c62af872c891c64c45151b5384fb7
Valentine <bmaecmpo@gmail.com>
2018-11-28 19:36:18 +0500

    Настраивает автоматизацию сборки проекта

---
+++ source/gulpfile.js
@@ -0,0 +1,5 @@
+var gulp = require("gulp");
+var less = require("gulp-less");
+var plumber = require("gulp-plumber");
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
